f97fbd7a5f83e331290f04735104626d81fd1fba
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-07-13 17:43:16 +0900

    added questions

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 // import u from './img/question/スクリーンショット 2022-06-24 .png'
+import cardiacNerves from './img/answer/cardiacNerves.png'
+import synthesizeCatecholamine from './img/answer/synthesizeCatecholamine.png'
 
 export const useQuestionList = () => {
   const [questionList, setQuestionList] = useState([
@@ -174,13 +176,16 @@ export const useQuestionList = () => {
         {
           detailInfo: 'レジュメ質問7/13',
           questionImg: [],
-          questionSentence: '緊張したときに、顔面蒼白、心悸亢進、体のほてりが生じるのはなぜか？',
+          questionSentence:
+            '緊張したときに、顔面蒼白、心悸亢進、体のほてりが生じるのはなぜか？',
           choices: [],
           answerImg: [],
-          answer: '交感神経からのノルアドレナリン分泌によって皮膚の血管が収縮し、心拍出量が増え、熱放出量が減り、ほてりが生じる。',
-          commentary: '皮膚の血管を収縮させるのはα受容体、心拍出量が増えるのはβ1受容体。',
+          answer:
+            '交感神経からのノルアドレナリン分泌によって皮膚の血管が収縮し、心拍出量が増え、熱放出量が減り、ほてりが生じる。',
+          commentary:
+            '皮膚の血管を収縮させるのはα受容体、心拍出量が増えるのはβ1受容体。',
         },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -189,7 +194,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -198,7 +203,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -207,7 +212,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -216,7 +221,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -225,7 +230,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -234,7 +239,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -243,7 +248,7 @@ export const useQuestionList = () => {
         //   answer: '',
         //   commentary: '',
         // },
-                // {
+        // {
         //   detailInfo: 'レジュメ質問7/13',
         //   questionImg: [],
         //   questionSentence: '',
@@ -277,6 +282,237 @@ export const useQuestionList = () => {
         //   choices: [],
         //   answerImg: [],
         //   answer: 'クラッシュ症候群、塩化カリウム製剤投与',
+        //   commentary: '',
+        // },
+      ],
+    },
+    {
+      groupTag: '2020中間(一問一答)',
+      groupContents: [
+        {
+          detailInfo: 'A(1)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'プルキンエ線維の伝達速度は、心室筋の伝達速度よりも速い',
+            '房室結節の伝達速度は、心房筋の伝達速度よりも速い',
+            '交感神経が興奮すると、洞房結節の興奮頻度が増加する',
+          ],
+          answerImg: [cardiacNerves],
+          answer:
+            'プルキンエ線維の伝達速度は、心室筋の伝達速度よりも速い/交感神経が興奮すると、洞房結節の興奮頻度が増加する',
+          commentary:
+            '神経伝達は径が大きいほど速い：プルキンエ線維>心筋>結節。心臓に豊富なβ1アドレナリン受容体は心機能亢進作用がある。',
+        },
+        {
+          detailInfo: 'A(2)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'カテコールアミンは、チロシンから合成される',
+            'ノルアドレナリンは、アドレナリンから合成される',
+            'L-ドーパは、パーキンソン病の治療薬として用いられる',
+          ],
+          answerImg: [synthesizeCatecholamine],
+          answer:
+            'カテコールアミンは、チロシンから合成される/L-ドーパは、パーキンソン病の治療薬として用いられる',
+          commentary:
+            'パーキンソン病は脳黒質でドーパミン産生が低下する病気であるが、ドーパミン自体は血液脳関門を通過できないためL-DOPAが治療で使われる',
+        },
+        {
+          detailInfo: 'A(3)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            '最低血圧が高くなると、前負荷が増大する',
+            '心室の拡張終期容量は、後負荷の指標になる',
+            'Frank-Starlingの法則によると、前負荷が増加すれば、一回拍出量も増加する',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(4)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            '1音は、房室弁が開放する際に聴取される',
+            '2音は、動脈弁の閉鎖時に聴取される',
+            '下半身のリンパ管は胸管に合流して、右静脈角で静脈に合流する',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(5)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'α1アドレナリン受容体の刺激は、末梢血管平滑筋を収縮させ、その結果拡張期血圧を上昇させる',
+            'β1アドレナリン受容体の刺激は、心拍出量を増加させる',
+            'β2アドレナリン受容体の刺激は、気管支平滑筋を収縮させる',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(6)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'Na-Kイオンポンプは、3個のNaイオンを排出して2個のKイオンを取り込む',
+            'Na-Kイオンポンプは、強心配糖体により活性化される',
+            'Na-Caイオン交換系では、3個のNaイオンと1個のCaイオンが交換される',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(7)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'P波は洞房結節の脱分極を反映する',
+            'QRS波は心室の脱分極を反映する',
+            '洞房結節の自動能が低下すると、PQ間隔が延長する',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(8)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            '心筋梗塞では、発症後30分から2時間程度経過した時点で、ST上昇を認める',
+            '心筋梗塞では、胸痛が消失した後も、異常Q波が残る',
+            'V1,V2,V3,V4で異常Q波を認めた際は、前壁中隔梗塞が疑われる',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(9)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            'Ⅱ誘導では、左手と左足の電位差を計測する',
+            'aVR誘導では、右手と左脚の電位差を計測する',
+            'V1の電極は、第5肋間胸骨右縁に装着する',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(10)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: [
+            '胎児では、右心室の血液は卵円孔を通過して左心室へ流入する',
+            '胎児では、肺動脈の血液が動脈管を通過して大動脈へ流入する',
+            '卵円孔は、生後六ヶ月程度で閉鎖して卵円窩となる',
+          ],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        {
+          detailInfo: 'A(1)',
+          questionImg: [],
+          questionSentence:
+            '正しい選択肢を選べ（複数選択可/全て正解・全て誤りの場合もある）',
+          choices: ['', '', ''],
+          answerImg: [],
+          answer: '',
+          commentary: '',
+        },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
+        //   commentary: '',
+        // },
+        // {
+        //   detailInfo: 'A(1)',
+        //   questionImg: [],
+        //   questionSentence: '',
+        //   choices: ['','',''],
+        //   answerImg: [],
+        //   answer: '',
         //   commentary: '',
         // },
       ],
